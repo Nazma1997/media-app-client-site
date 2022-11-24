17e8aecd3bb7ce33e3a0172a70f87471101b885c
Nazma1997 <nazma.ist.707@gmail.com>
2022-11-24 21:43:00 +0600

    added

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,43 @@
+import {React, useState, useEffect}from 'react';
+// import {useSelector} from 'react-redux';
+import axios from 'axios';
+import {Grid, CircularProgress} from '@material-ui/core'
+import useStyles from './styles';
+import Post from './Post/Post.js'
+const Posts = () => {
+  // const posts = useSelector((state) => state.posts);
+
+  const [posts, setPosts] = useState([]);
+
+
+  useEffect(() => {
+       loadUsers()
+  },[]);
+
+
+  const loadUsers = async() => {
+       const result =await axios.get("http://localhost:4000/api/v1/posts");
+        setPosts(result.data)
+      //  console.log(result)
+  }
+
+  console.log(posts)
+  const classes = useStyles();
+  return (
+    // !posts.length ? <CircularProgress />
+   !posts.length ? <CircularProgress /> : (
+    <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+      {
+        posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} lg={6}>
+              <Post  post={post}/>
+          </Grid>
+        ) )
+      }
+        
+    </Grid>
+   )
+  )
+}
+
+export default Posts
---
+++ src/reducers/posts.js
@@ -0,0 +1,49 @@
+const posts = (posts = [], action) => {
+  switch(action.type){
+    case 'FETCH_ALL':
+      // return action.payload;
+      return  action.payload;
+
+    case 'CREATE':
+      return [...posts, action.payload];
+
+    default: 
+      return posts;
+  }
+}
+
+export default posts;
+
+
+
+// import {createSlice} from '@reduxjs/toolkit';
+
+
+
+// const posts = createSlice({
+//    name: 'posts',
+//    initialState: {
+//     posts:[],
+//     isFetching: false,
+//     error: false
+//    },
+//    reducers:{
+//     getPosts: (posts=[], action)=>{
+      
+//     switch(action.type){
+//     case 'FETCH_ALL':
+//       // return action.payload;
+//       return  action.payload;
+
+//     case 'CREATE':
+//       return [...posts, action.payload];
+
+//     default: 
+//       return posts;
+//   }
+//     }
+//    }
+// });
+
+
+// export default posts;
