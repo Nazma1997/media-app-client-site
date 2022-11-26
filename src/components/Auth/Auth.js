668324ebb3c928fb713bcb15d396ca9f251e7f60
Nazma1997 <nazma.ist.707@gmail.com>
2022-11-26 08:32:49 +0600

    added

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,16 +1,53 @@
 import React from 'react';
 import{Avatar, Button, Paper, Grid, Typography, Container} from '@material-ui/core';
+import LockOutlineIcon from '@material-ui/icons/LockOpenOutlined';
 import useStyles from './style';
+import Input from './Input';
 
 
 const Auth = () => {
-
   const classes = useStyles();
-  (
+
+  const isSignup = false;
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = () => {
+
+  }
+
+  const handleShowPassword = ()  => {
+
+  }
+
+  return(
     <Container component='main' maxWidth='xs'>
-      <Paper>
-      
+      <Paper className={classes.paper} elevation={3}>
+         <Avatar className={classes.avatar}>
+           <LockOutlineIcon />
+         </Avatar>
+         <Typography variant='h5' >{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+         <form className={classes.form} onSubmit= {handleSubmit}>
+          <Grid container spacing={2}>
+             {
+              isSignup && (
+                <>
+                   <Input name='firstName' label='First Name' handleChange={handleChange} autoFocus half/>
+                   <Input name='lastName' label='Last Name' handleChange={handleChange} half/>
+                </>
+              )
+             }
+             <Input name='email' label='Email Address' handleChange={handleChange} type='email'/>
+             <Input name='password' label='Password' handleChange={handleChange} type='password' handleShowPassword={handleShowPassword}/>
+          </Grid> 
+             
+         </form>
+         
       </Paper>
+
+    
 
     </Container>
   )
